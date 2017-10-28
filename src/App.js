d587ae3a52e28a992cb6b982623c0a2b3b4cd20d
wangandai <wang0901@e.ntu.edu.sg>
2017-10-28 13:56:05 +0800

    Added antd

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
 import './App.css';
 
+const { Header, Content, Footer } = Layout;
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <Header style={{ position: 'fixed', width: '100%' }}>
+                        <Menu theme="light" mode="horizontal" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">About Me</Menu.Item>
+                            <Menu.Item key="2">Projects</Menu.Item>
+                            <Menu.Item key="3">Resume</Menu.Item>
+                            <Menu.Item key="4">Contact Me</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content style={{ width: '100%', marginTop: 64 }}>Content</Content>
+                    <Footer>Footer</Footer>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
